d2bd3cb1e558ca77ab854b1259c3bcbe4a9912be
rckl0320 <130993661+rckl0320@users.noreply.github.com>
2023-05-03 09:07:40 -0400

    made bear game

--- MyWorld.java
+++ MyWorld.java
@@ -13,9 +13,12 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        Bear sjo = new Bear();
+        addObject(sjo, 300, 200);
     }
 }
