ee4b065f7390710e5d7c62459ecc9aee6bb8d40e
rckl0320 <130993661+rckl0320@users.noreply.github.com>
2023-05-26 09:15:48 -0400

    added left and right animation

--- Bear.java
+++ Bear.java
@@ -9,31 +9,79 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Bear extends Actor
 {
     GreenfootSound breadEat = new GreenfootSound("good.mp3");
-    GreenfootImage idleWalk = new GreenfootImage("images/bear_walk/bearWalk0.png");
+    GreenfootImage[] idleWalkRight = new GreenfootImage[4];
+    GreenfootImage[] idleWalkLeft = new GreenfootImage[4];
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    //Direction storing variable
+    String facing = "right";
     /**
      * Act - do whatever the Bear wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
-    //constructor   
+    /**
+     * constructor
+     */   
     public Bear()
     {
-        setImage(idleWalk);
+        for(int i=0; i<idleWalkRight.length; i++)
+        {
+            idleWalkRight[i] = new GreenfootImage("images/bear_walk/bearWalk" + i + ".png");
+            idleWalkRight[i].scale(120, 90);
+        }
+        
+        for(int i=0; i<idleWalkLeft.length; i++)
+        {
+            idleWalkLeft[i] = new GreenfootImage("images/bear_walk/bearWalk" + i + ".png");
+            idleWalkLeft[i].mirrorHorizontally();
+            idleWalkLeft[i].scale(120, 90);
+        }
+        
+        animationTimer.mark();
+        
+        setImage(idleWalkRight[0]);
     }
     
+    /**
+     * Animates bear
+     */
+    int imageIndex = 0;
+    public void animateBear()
+    {
+        if(animationTimer.millisElapsed() < 25)
+        {  
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right"))
+        {
+            setImage(idleWalkRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleWalkRight.length;
+        }
+        else
+        {
+            setImage(idleWalkLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleWalkLeft.length; 
+        }
     
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
             move(-5);
+            facing = "left";
         }
         
         if(Greenfoot.isKeyDown("right")){
             move(5);
+            facing = "right";
         }
         
-        
-        eat();  
+        //activate eating
+        eat(); 
+        //activate animations
+        animateBear();
     }
     //eat bread and spawn new bread
     public void eat()
